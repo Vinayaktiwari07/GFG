d44000abfa1fcdcb2e872fd78b27a7b975003a10
Vinayaktiwari07 <vinayaktiwari00326@gmail.com>
2025-11-15 18:49:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+  public:
+  bool isPossibleSolution(vector<int>& a, int n, int k, long long mid){
+    //   int n = arr.size();
+      long long timesum = 0;
+      int c = 1;
+      
+      for(int i=0; i<n; i++){
+          if(a[i] > mid){
+              return false;
+          }
+          if(a[i] + timesum > mid){
+              c++;
+              timesum = a[i];
+              if(c > k) return false;
+          }
+          else{
+              timesum += a[i];
+          }
+      }
+      return true;
+      
+  }
+  
+    int minTime(vector<int>& arr, int k) {
+        int n = arr.size();
+        long long start = 0;
+        long long end = 0;
+        
+        for(int i=0; i<n; i++){
+            end += arr[i];
+        }
+        long long ans = -1;
+        
+        while(start <= end){
+            long long mid = start + (end - start)/2;
+            if(isPossibleSolution(arr, n, k, mid)){
+                ans = mid;
+                end = mid - 1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return ans;
+        
+    }
+};
